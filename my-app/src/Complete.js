25795c83cf8da53c95c4e04c56006619b276e06f
yjk5309 <yunjookim97@daum.net>
2021-02-26 17:59:58 +0900

    컴포넌트 합치기, post로 데이터 받아오기 성공 받아온 데이터 넘겨주어야함

--- my-app/src/Complete.js
+++ my-app/src/Complete.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import {Button} from 'react-bootstrap';
 
 export default function Complete() {
@@ -7,7 +7,11 @@ export default function Complete() {
     return(
         <div>
             <h2>검사가 완료되었습니다</h2>
-            <Button>결과보기</Button>
+            <p>
+            검사결과는 여러분이 직업을 선택할 때 상대적으로 어떠한 가치를 중요하게 생각하는지를 알려주고,
+            중요 가치를 충족시켜줄 수 있는 직업에 대해 생각해 볼 기회를 제공합니다.
+            </p>
+            <Link><Button>결과보기</Button></Link>
         </div>
     )
 }
--- my-app/src/User.js
+++ my-app/src/User.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import './Test.css';
 
 export default function User() {
-
+    //이름 성별
     const [name, setName] = useState('');
     const [gender, setGender] = useState('');
 
@@ -17,23 +17,25 @@ export default function User() {
         setGender(e.target.value);
     }
 
+    //검사 예시
+    const [checked, setChecked] = useState('');
+
+    function handleCheck(e){
+        setChecked(e.target.value);
+    }
+
     var apiUrl = `http://www.career.go.kr/inspct/openapi/test/questions?apikey=0ae61054823ff25204fc658195732555&q=6`
 
+    //검사 진행
     const [questionList, setQuestionList] = useState([]);
-    const [answers, setAnswers] = useState({id:'', answer:''});
+    const [answers, setAnswers] = useState([]);
     const [page, setPage] = useState(-2);
 
-    const handleAnswer = e =>{
-        setAnswers(state =>({...state, [e.target.name]: e.target.value}));
-      }
-    
-    function handleNextPage(){
-        setPage(page+1)
-    }
-
-    function handlePrevPage(){
-        setPage(page-1)
-    }
+    const handleAnswer = (e) => {
+        let result = [...answers];
+        result[e.target.name - 1] = e.target.value; 
+        setAnswers(result); 
+      };
 
     const fetchQuestions = useCallback(async () =>{
         const response = await axios.get(apiUrl);
@@ -43,55 +45,68 @@ export default function User() {
     useEffect(() => {
         fetchQuestions();
       }, [fetchQuestions]);
-    
-    const q = questionList.map(
-        (questionList) => (<div>
-            <div id="question">
-                <p>{questionList.question}</p>
-                <div>
-                    <input type='radio' name={`B${questionList.qitemNo}`} value={questionList.answerScore01}
-                     onChange={handleAnswer} />{questionList.answer01}
-                    <input type='radio' name={`B${questionList.qitemNo}`} value={questionList.answerScore02}
-                     onChange={handleAnswer} />{questionList.answer02}
-                </div>
-            </div>
-        </div>)
-    )
+
+      function handleNextPage(){
+        setPage(page+1);
+    }
+
+    function handlePrevPage(){
+        setPage(page-1)
+    }
 
     const visibleQuestion = useMemo(() => {
-        return q.slice(page*5,(page+1)*5)
-    },[page,q]);
+        return questionList.slice(page*5,(page+1)*5)
+    },[page,questionList]);
+
+    const exampleQuestion = useMemo(() => {
+        return questionList[0]
+    },[questionList]);
+
+    console.log(name);
+    console.log(gender);
     console.log(answers);
 
-    // const isButtonDisabled = useMemo(() => {
-    //     let isDisabled = false;
-    //     visibleQuestion.forEach((question) => {
-    //       const index = parseInt(question.qitemNo, 10);
-    //       if (!answer[index - 1]) {
-    //         isDisabled = true;
-    //       }
-    //     });
-    //     return isDisabled;
-    //   }, [answer, visibleQuestion]);
-    function Tests(visibleQuestion){
-        return(
-            <div>
-            검사시작
-            {visibleQuestion}
-            {page ?
-            <Button onClick={handlePrevPage}>이전</Button>
-            : <Link to='/example'><Button onClick={handlePrevPage}>이전</Button></Link>}
-            {page === 5 ?
-              <Link to='/completion'>
-                  <Button onClick={handleNextPage}>완료</Button></Link>
-            :  <Button onClick={handleNextPage}>다음</Button>
-            }
-            </div>
-        )
-    }
-    
+    const isButtonDisabled = useMemo(() => {
+        let isDisabled = false;
+        visibleQuestion.forEach((question) => {
+          const index = parseInt(question.qitemNo, 10);
+          if (!answers[index - 1]) {
+            isDisabled = true;
+          }
+        });
+        return isDisabled;
+      }, [answers, visibleQuestion]);
 
-    console.log(page);
+    const handleSubmit = e =>{
+        e.preventDefault();
+        var formatAnswers = "";
+
+        for(var i =0;i<answers.length;i++){
+            var answer = "B"+(i+1).toString()+"="+answers[i].toString()+" ";
+            formatAnswers+=answer;
+        }
+        console.log(formatAnswers);
+        var timestamp = new Date().getTime();
+        var data = { 
+            "apikey": "0ae61054823ff25204fc658195732555",
+            "qestrnSeq": "6", //검사번호
+            "trgetSe": "100209", //일반인
+            "name": name,
+            "gender": gender,
+            "startDtm": timestamp.toString(),
+            "answers": formatAnswers
+        }
+        console.log(data);
+        
+        async function responseGet(){
+            const response = await axios.post(`http://www.career.go.kr/inspct/openapi/test/report`, 
+            data, {headers: {'Content-Type': 'application/json'}});
+            console.log(response.data.RESULT);
+    }
+    responseGet();
+    }
+    //post로 데이터를 받아오기까지 성공했는데 받아온 것들을 넘겨주는 방법?(props? useParams?)
+
     return(
         <>
         <div>
@@ -106,7 +121,53 @@ export default function User() {
                 <Button disabled={!name || !gender} onClick={handleNextPage}>검사 시작</Button>
             </div>}
             {page === -1 &&
-             <Tests />
+             <div>
+             <h2>검사 예시</h2>
+             <h4>{exampleQuestion.question}</h4>
+            <div>
+                <label>
+                <input type='radio' name='answer' value='1' onChange={handleCheck} />{exampleQuestion.answer01}
+                </label>
+                <label>
+                <input type='radio' name='answer' value='2' onChange={handleCheck} />{exampleQuestion.answer02}
+                </label>
+            </div>
+             <Button onClick={handlePrevPage}>이전</Button>
+             <Button onClick={handleNextPage} disabled={!checked}>다음</Button>
+             </div>
+            }
+            {page >= 0 &&
+            <div>
+            <h2>검사 진행</h2>
+            {visibleQuestion.map(
+                (visibleQuestion) => (<div>
+                    <div id="question">
+                        <p>{visibleQuestion.question}</p>
+                        <div>
+                            <label>
+                            <input type='radio' name={visibleQuestion.qitemNo}
+                             value={visibleQuestion.answerScore01}
+                             onChange={handleAnswer}
+                             checked = {answers[visibleQuestion.qitemNo-1] === visibleQuestion.answerScore01 } />
+                             {visibleQuestion.answer01}</label>
+                            <label>
+                            <input type='radio' name={visibleQuestion.qitemNo}
+                             value={visibleQuestion.answerScore02}
+                             onChange={handleAnswer} 
+                             checked = {answers[visibleQuestion.qitemNo-1] === visibleQuestion.answerScore02 }/>
+                            {visibleQuestion.answer02}</label>
+                        </div>
+                    </div>
+                </div>)
+            )
+            }
+            <Button onClick={handlePrevPage}>이전</Button>
+            {page === 5 ?
+              <Link to='/completion'>
+                  <Button onClick={handleSubmit} disabled={isButtonDisabled}>완료</Button></Link>
+            :  <Button onClick={handleNextPage} disabled={isButtonDisabled}>다음</Button>
+            }
+            </div>
         }
             
         </div>
